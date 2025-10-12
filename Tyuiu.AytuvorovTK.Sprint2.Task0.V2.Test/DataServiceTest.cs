78f0637a14a0fff9a01c20ef92c5d9505e88059f
dimka <dimka556.dim4@gmail.com>
2025-10-12 16:49:18 +0500

    Add project files.

---
+++ Tyuiu.AytuvorovTK.Sprint2.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AytuvorovTK.Sprint2.Task0.V2.Lib;
+
+namespace Tyuiu.AytuvorovTK.Sprint2.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 123;
+            int y = 123;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AytuvorovTK.Sprint2.Task0.V2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvorovTK.Sprint2.Task0.V2.Lib;
+
+
+namespace Tyuiu.AytuvorovTK.Sprint2.Task0.V2;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        DataService ds = new DataService();
+
+        int x = 123;
+        int y = 123;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+            Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+            Console.WriteLine("X = : " + x);
+            Console.WriteLine("Y = : " + y);
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
