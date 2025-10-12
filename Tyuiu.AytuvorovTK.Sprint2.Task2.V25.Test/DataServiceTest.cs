371e40a810d49c6d9fa25cecfe3fdf5374032d94
dimka <dimka556.dim4@gmail.com>
2025-10-12 17:57:30 +0500

    Task2

---
+++ Tyuiu.AytuvorovTK.Sprint2.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AytuvorovTK.Sprint2.Task2.V25.Lib;
+namespace Tyuiu.AytuvorovTK.Sprint2.Task2.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AytuvorovTK.Sprint2.Task2.V25/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvorovTK.Sprint2.Task2.V25.Lib;
+
+
+namespace Tyuiu.AytuvorovTK.Sprint2.Task2.V25;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+        Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+        Console.WriteLine(" Введите значение переменной X: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(" Введите значение переменной Y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+        Console.WriteLine("*********************************************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+        Console.WriteLine("*********************************************************************************************************************");
+
+
+        if (res)
+        {
+            Console.WriteLine(" Точка находится в заштрихованной области ");
+        }
+        else
+        {
+            Console.WriteLine(" Точка не находится в заштрихованной области ");
+        }
+
+        Console.ReadKey();
+
+    }
+}
