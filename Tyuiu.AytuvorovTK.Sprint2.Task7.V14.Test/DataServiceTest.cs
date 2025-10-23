4c2718d6f7daa3f5796cbcf8064f3d431638b9a9
dimka <dimka556.dim4@gmail.com>
2025-10-23 22:42:52 +0500

    Tas7

--- Tyuiu.AytuvorovTK.Sprint2.Task7.V14.Test/DataServiceTest.cs
+++ Tyuiu.AytuvorovTK.Sprint2.Task7.V14.Test/DataServiceTest.cs
@@ -9,11 +9,11 @@ namespace Tyuiu.AytuvorovTK.Sprint2.Task7.V14.Test
         {
             DataService ds = new DataService();
 
-            double x = 0;
+            double x = 0.1;
             double y = 0;
 
             bool res = ds.CheckDotInShadedArea(x, y);
-            bool wait = true;
+            bool wait = false;
 
             Assert.AreEqual(wait, res);
         }
