78065a3f75e5ad830f8796ab6c98e1957a35c362
dimka <dimka556.dim4@gmail.com>
2025-10-23 22:38:55 +0500

    Task7

---
+++ Tyuiu.AytuvorovTK.Sprint2.Task7.V14.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AytuvorovTK.Sprint2.Task7.V14.Lib
+{
+    public class DataService : ISprint2Task7V14
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            return (x * x + y * y <= 1) && (y >= x) && (y <= -x);
+        }
+    }
+}
---
+++ Tyuiu.AytuvorovTK.Sprint2.Task7.V14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvorovTK.Sprint2.Task7.V14.Lib;
+
+
+namespace Tyuiu.AytuvorovTK.Sprint2.Task7.V14;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                                                                                                                                            *");
+        Console.WriteLine("***********************************************************************************************************************************************************************************************************************************************");
+
+        Console.WriteLine(" Введите значение X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(" Введите значение Y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+        Console.WriteLine("*********************************************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+        Console.WriteLine("*********************************************************************************************************************");
+
+        if (res)
+        {
+            Console.WriteLine("Точка находится в закрашенной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка находится не в закрашенной области");
+        }
+
+        Console.ReadKey();
+
+    }
+}
