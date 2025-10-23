bc5ea1043adf36e1f47580174f8bcced0ef15bdd
dimka <dimka556.dim4@gmail.com>
2025-10-23 21:45:25 +0500

    ta

--- Tyuiu.AytuvorovTK.Sprint2.Task5.V14.Test/DataServiceTest.cs
+++ Tyuiu.AytuvorovTK.Sprint2.Task5.V14.Test/DataServiceTest.cs
@@ -10,13 +10,13 @@ namespace Tyuiu.AytuvorovTK.Sprint2.Task5.V14.Test
             DataService ds = new DataService();
 
 
-            Assert.AreEqual("понедельник", ds.FindDayName(1, 8));
-            Assert.AreEqual("вторник", ds.FindDayName(2, 1));
-            Assert.AreEqual("воскресенье", ds.FindDayName(7, 1));  
-            Assert.AreEqual("понедельник", ds.FindDayName(8, 1));  
-            Assert.AreEqual("понедельник", ds.FindDayName(1, 1));  
-            Assert.AreEqual("вторник", ds.FindDayName(2, 1));      
-            Assert.AreEqual("воскресенье", ds.FindDayName(7, 1));
+            Assert.AreEqual("Понедельник", ds.FindDayName(1, 8));
+            Assert.AreEqual("Вторник", ds.FindDayName(2, 1));
+            Assert.AreEqual("Воскресенье", ds.FindDayName(7, 1));  
+            Assert.AreEqual("Понедельник", ds.FindDayName(8, 1));  
+            Assert.AreEqual("Понедельник", ds.FindDayName(1, 1));  
+            Assert.AreEqual("Вторник", ds.FindDayName(2, 1));      
+            Assert.AreEqual("Воскресенье", ds.FindDayName(7, 1));
 
         }
     }
